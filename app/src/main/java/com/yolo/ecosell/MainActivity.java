9aa66dd40b12fb3b00229192f0e2c602c8b8eb7a
SteveDexter24 <tamangsteve@gmail.com>
2022-05-10 02:46:50 +0800

    Navigate to login activity

--- app/src/main/java/com/yolo/ecosell/MainActivity.java
+++ app/src/main/java/com/yolo/ecosell/MainActivity.java
@@ -2,13 +2,25 @@ package com.yolo.ecosell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button getStartedButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getStartedButton = findViewById(R.id.start_button);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go to login activity
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
