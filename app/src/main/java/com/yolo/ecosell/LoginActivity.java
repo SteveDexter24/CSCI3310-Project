30d051487fc7d621f72a24bbbc850e4bf71c8c2d
SteveDexter24 <tamangsteve@gmail.com>
2022-05-10 02:47:03 +0800

    navigate to create account activity

---
+++ app/src/main/java/com/yolo/ecosell/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.yolo.ecosell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button createAccount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        createAccount = findViewById(R.id.link_to_signup);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+            }
+        });
+    }
+
+}
