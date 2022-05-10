73994d120c11e6500ee097b5bb8652b42f718fe9
SteveDexter24 <tamangsteve@gmail.com>
2022-05-11 04:13:09 +0800

    Add/connect UI from xml to login actvity

--- app/src/main/java/com/yolo/ecosell/LoginActivity.java
+++ app/src/main/java/com/yolo/ecosell/LoginActivity.java
@@ -1,20 +1,56 @@
 package com.yolo.ecosell;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import model.User;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button createAccount;
+    private Button createAccount, signInButton, signInWithGoogleButton;
+    private EditText emailEditText, passwordEditText;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
         createAccount = findViewById(R.id.link_to_signup);
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        progressBar = findViewById(R.id.login_progress);
+        signInButton = findViewById(R.id.email_sign_in_button);
         goToCreateAccountActivity();
+        loginWithEmailAndPassword();
     }
     private void goToCreateAccountActivity(){
         createAccount.setOnClickListener(new View.OnClickListener() {
