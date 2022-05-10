022c959deb0b476cddfc80f912d1642b1b873120
SteveDexter24 <tamangsteve@gmail.com>
2022-05-11 02:08:43 +0800

    remove imageview class

--- app/src/main/java/com/yolo/ecosell/CreateAccountActivity.java
+++ app/src/main/java/com/yolo/ecosell/CreateAccountActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
