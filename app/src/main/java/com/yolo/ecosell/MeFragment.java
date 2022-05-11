ce3c7317793725daeb181dc05f96144968bf5cb1
SteveDexter24 <tamangsteve@gmail.com>
2022-05-12 05:03:39 +0800

    Create MeFragment for user acc page

---
+++ app/src/main/java/com/yolo/ecosell/MeFragment.java
@@ -0,0 +1,41 @@
+package com.yolo.ecosell;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MeFragment extends Fragment {
+
+    public MeFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static MeFragment newInstance(String param1, String param2) {
+        MeFragment fragment = new MeFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_me, container, false);
+    }
+}
