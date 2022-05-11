888ba7b00cd59e1be16381d22760e48d7e717685
SteveDexter24 <tamangsteve@gmail.com>
2022-05-12 02:41:04 +0800

    Remove user view model

--- app/src/main/java/com/yolo/ecosell/MainActivity.java
+++ app/src/main/java/com/yolo/ecosell/MainActivity.java
@@ -1,19 +1,15 @@
 package com.yolo.ecosell;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import model.UserViewModel;
 
 public class MainActivity extends AppCompatActivity {
     private Button getStartedButton;
-    private UserViewModel userViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +22,6 @@ public class MainActivity extends AppCompatActivity {
                 // Go to login activity
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
-        });
-
-        userViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication())
-                .create(UserViewModel.class);
-
-        userViewModel.getAllUsers().observe(this, users -> {
-            Log.d("MainActivity", "onCreate: " + users.get(0).getUsername());
         });
 
     }
