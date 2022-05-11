3bc7d88d0ee5a765a7014d4c38fcfb855cd4bbae
SteveDexter24 <tamangsteve@gmail.com>
2022-05-11 17:47:47 +0800

    Revert "Merge pull request #26 from SteveDexter24/yuri"
    This reverts commit 5ec80916f39218f6fa70065d5573b7a80ea79b8e, reversing
    changes made to 8207bbe8d4213176c2273d0a76654637ed7dab66.

--- app/src/main/java/com/yolo/ecosell/HomeActivity.java
+++ app/src/main/java/com/yolo/ecosell/HomeActivity.java
@@ -1,37 +1,15 @@
 package com.yolo.ecosell;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.yolo.ecosell.databinding.ActivityMainBinding;
-
 public class HomeActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_home);
-
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_explore, R.id.navigation_dashboard, R.id.navigation_notifications)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_home);
     }
 }
-
