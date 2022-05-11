0243a6e2ba1240354bdcc9ed6d0d00bb92a3c0ca
SteveDexter24 <tamangsteve@gmail.com>
2022-05-11 18:27:16 +0800

    Create User repo to perform crud, get all users

---
+++ app/src/main/java/data/UserRepository.java
@@ -0,0 +1,26 @@
+package data;
+
+import android.app.Application;
+import android.text.style.IconMarginSpan;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import model.User;
+import util.EcoSellRoomDatabase;
+
+public class UserRepository {
+    private UserDao userDao;
+    private LiveData<List<User>> allUsers;
+    private LiveData<List<User>> searchUsers;
+
+    public UserRepository(Application application) {
+        EcoSellRoomDatabase db = EcoSellRoomDatabase.getDatabase(application);
+        userDao = db.userDao();
+        allUsers = userDao.getAllUsers();
+    }
+
+    public LiveData<List<User>> getAllUsers() { return allUsers; }
+
+}
