faf32d704870ac8d507b39d4a4e14fd943335153
yuri <yurichan@YurideMacBook-Air.local>
2022-05-11 18:47:36 +0800

    add search bar, home activity xml crash :(

--- app/src/main/java/com/yolo/ecosell/HomeActivity.java
+++ app/src/main/java/com/yolo/ecosell/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.yolo.ecosell;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,12 +19,22 @@ import com.yolo.ecosell.databinding.ActivityMainBinding;
 
 public class HomeActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    ListView listView;
+    String[] items = {"Outers", "Tops", "Dresses","Skirts","Shorts", "Pants","Shoes","Bags","Accessories"};
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_home);
 
+                //search bar
+                listView = findViewById(R.id.listview);
+                arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+                listView.setAdapter(arrayAdapter);
+
+//        recycleview for explore
+//
 //        binding = ActivityMainBinding.inflate(getLayoutInflater());
 //        setContentView(binding.getRoot());
 //
@@ -33,5 +48,35 @@ public class HomeActivity extends AppCompatActivity {
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 //        NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
+
+    //search bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.explore_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
+
+
 
