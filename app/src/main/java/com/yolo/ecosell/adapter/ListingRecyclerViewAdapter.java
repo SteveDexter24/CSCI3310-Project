a9af34c40455d1f70593571d1271ecd1edc12f1c
SteveDexter24 <tamangsteve@gmail.com>
2022-05-13 02:09:04 +0800

    Initialise listing recycler view adapter

---
+++ app/src/main/java/com/yolo/ecosell/adapter/ListingRecyclerViewAdapter.java
@@ -0,0 +1,65 @@
+package com.yolo.ecosell.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.yolo.ecosell.R;
+
+public class ListingRecyclerViewAdapter extends RecyclerView.Adapter<ListingRecyclerViewAdapter.ListingsViewHolder> {
+
+    //private LiveData<List<>>
+    private Context context;
+
+    public ListingRecyclerViewAdapter(Context context){
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ListingsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_listing_row, parent, false);
+        return new ListingsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListingsViewHolder holder, int position) {
+        holder.priceTextView.setText("HK$100");
+        holder.productNameTextView.setText("Pants");
+        holder.usageTextView.setText("90% New");
+        holder.sellerTextView.setText(("DummyUser1234"));
+        Glide.with(holder.itemView.getContext())
+                .load("https://images.lululemon.com/is/image/lululemon/LM5ADRS_0001_1?size=800,800").into(holder.productImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public class ListingsViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView productImageView, sellerImageView;
+        private TextView productNameTextView, priceTextView, usageTextView, sellerTextView;
+
+        public ListingsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productImageView = itemView.findViewById(R.id.listing_imageView);
+            sellerImageView = itemView.findViewById(R.id.listing_user_profile_imageView);
+
+            productNameTextView = itemView.findViewById(R.id.listing_title_textView);
+            priceTextView = itemView.findViewById(R.id.listing_price_textView);
+            usageTextView = itemView.findViewById(R.id.listing_usage_textView);
+            sellerTextView = itemView.findViewById(R.id.listing_user_textView);
+        }
+    }
+}
