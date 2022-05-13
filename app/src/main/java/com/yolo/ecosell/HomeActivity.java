f588a7ae48002eae5905cc03614a02df041def6d
yuri <yurichan@YurideMacBook-Air.local>
2022-05-13 18:35:45 +0800

    remove wrong search bar related part in menu layout and Home activity

--- app/src/main/java/com/yolo/ecosell/HomeActivity.java
+++ app/src/main/java/com/yolo/ecosell/HomeActivity.java
@@ -25,10 +25,6 @@ import model.UserViewModel;
 
 public class HomeActivity extends AppCompatActivity {
 
-//    ListView listView;
-//    String[] items = {"Outers", "Tops", "Dresses","Skirts","Shorts", "Pants","Shoes","Bags","Accessories"};
-//    ArrayAdapter<String> arrayAdapter;
-
     private UserViewModel userViewModel;
     BottomNavigationView bottomNavigationView;
     ExploreFragment exploreFragment = new ExploreFragment();
@@ -75,54 +71,11 @@ public class HomeActivity extends AppCompatActivity {
                 }
             });
         });
-        //search bar
-//                listView = findViewById(R.id.listview);
-//                arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-//                listView.setAdapter(arrayAdapter);
 
-//        recycleview for explore
-//
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_explore, R.id.navigation_dashboard, R.id.navigation_notifications)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
 
-    //search bar
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.search_menu, menu);
-//
-//        MenuItem menuItem = menu.findItem(R.id.explore_search);
-//        SearchView searchView = (SearchView) menuItem.getActionView();
-//        searchView.setQueryHint("Type here to search");
-//
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//                arrayAdapter.getFilter().filter(newText);
-//                return false;
-//            }
-//        });
-//
-//
-//        return super.onCreateOptionsMenu(menu);
-//    }
+
 }
 
 
