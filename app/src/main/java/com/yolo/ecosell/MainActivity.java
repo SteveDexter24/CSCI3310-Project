db13dd01e7b434426438b7b441205edbb17529ec
SteveDexter24 <tamangsteve@gmail.com>
2022-05-15 05:34:50 +0800

    if user is loggedIn then skip the login

--- app/src/main/java/com/yolo/ecosell/MainActivity.java
+++ app/src/main/java/com/yolo/ecosell/MainActivity.java
@@ -7,14 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button getStartedButton;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getStartedButton = findViewById(R.id.start_button);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
-
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+        }
     }
 }
