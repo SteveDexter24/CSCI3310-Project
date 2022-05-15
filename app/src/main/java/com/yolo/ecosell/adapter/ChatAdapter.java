ca683cf433352253a324eaf55f51639fd5ac32f4
SteveDexter24 <tamangsteve@gmail.com>
2022-05-15 23:18:03 +0800

    set up chat adapter

--- app/src/main/java/com/yolo/ecosell/adapter/ChatAdapter.java
+++ app/src/main/java/com/yolo/ecosell/adapter/ChatAdapter.java
@@ -2,6 +2,7 @@ package com.yolo.ecosell.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,15 +14,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.yolo.ecosell.ChatActivity;
 import com.yolo.ecosell.R;
 
+import java.util.List;
+
+import model.ChatRoom;
+
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
 
+    private static final String TAG = "ChatAdapter";
     private Context context;
+    private List<ChatRoom> chatRoomList;
+    private String chatRoomId;
 
-    public ChatAdapter(Context context){
+    public ChatAdapter(Context context, List<ChatRoom> chatRoomList, String chatRoomId) {
         this.context = context;
+        this.chatRoomList = chatRoomList;
+        this.chatRoomId = chatRoomId;
     }
 
     @NonNull
@@ -34,23 +46,42 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
-        holder.usernameTextView.setText("DummyUser");
-        holder.lastMessageTextView.setText("hahaha");
-        holder.chatUnseenTextView.setText("1");
-        Glide.with(holder.itemView.getContext())
-                .load("https://images.lululemon.com/is/image/lululemon/LM5ADRS_0001_1?size=800,800").into(holder.profileImageView);
 
-        holder.rootLinearLayout.setOnClickListener(view -> {
-            Intent intent = new Intent(context, ChatActivity.class);
-            intent.putExtra("username", "DummyUser");
-            context.startActivity(intent);
+        chatRoomList.get(position).getOtherUser().get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    Log.d(TAG, "DocumentSnapshot data: " + document.getData().get("username"));
+                    String username = (String) document.getData().get("username");
+                    String profileImage = (String) document.getData().get("imageUrl");
+                    holder.usernameTextView.setText(username);
+                    holder.lastMessageTextView.setText("hahaha");
+                    holder.chatUnseenTextView.setText("1");
+                    Glide.with(holder.itemView.getContext())
+                            .load(profileImage).into(holder.profileImageView);
+
+                    holder.rootLinearLayout.setOnClickListener(view -> {
+                        Intent intent = new Intent(context, ChatActivity.class);
+                        intent.putExtra("username", username);
+                        intent.putExtra("profileImage", profileImage);
+                        intent.putExtra("chatRoomId", chatRoomId);
+                        context.startActivity(intent);
+                    });
+                } else {
+                    Log.d(TAG, "No such document");
+                }
+            }
         });
+
+    }
+
+    private void getOtherUserDetails(DocumentReference otherUser) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 1;
+        return chatRoomList.size();
     }
 
     static class ChatViewHolder extends RecyclerView.ViewHolder {
@@ -61,6 +92,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
 
         public ChatViewHolder(@NonNull View itemView) {
             super(itemView);
+
             profileImageView = itemView.findViewById(R.id.chat_profile_image);
             usernameTextView = itemView.findViewById(R.id.chat_username_textView);
             lastMessageTextView = itemView.findViewById(R.id.chat_last_message_textView);
