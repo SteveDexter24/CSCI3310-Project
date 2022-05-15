f65d7af9cf7043f6d11b963d9a4def0b5ec330f1
SteveDexter24 <tamangsteve@gmail.com>
2022-05-15 23:21:28 +0800

    add user1 and user2 to chat room

--- app/src/main/java/model/ChatRoom.java
+++ app/src/main/java/model/ChatRoom.java
@@ -5,22 +5,27 @@ import com.google.firebase.firestore.DocumentReference;
 import java.util.List;
 
 public class ChatRoom {
-    private DocumentReference user;
+    private String user1;
+    private String user2;
     private DocumentReference otherUser;
     private List<DocumentReference> chats;
 
-    public ChatRoom(DocumentReference user, DocumentReference otherUser, List<DocumentReference> chats) {
-        this.user = user;
+    public ChatRoom () {
+    }
+
+    public ChatRoom(String user1, String user2, DocumentReference otherUser, List<DocumentReference> chats) {
+        this.user1 = user1;
+        this.user2 = user2;
         this.otherUser = otherUser;
         this.chats = chats;
     }
 
-    public DocumentReference getUser() {
-        return user;
+    public String getUser1() {
+        return user1;
     }
 
-    public void setUser(DocumentReference user) {
-        this.user = user;
+    public void setUser1(String user1) {
+        this.user1 = user1;
     }
 
     public DocumentReference getOtherUser() {
@@ -37,5 +42,13 @@ public class ChatRoom {
 
     public void setChats(List<DocumentReference> chats) {
         this.chats = chats;
+    }
+
+    public String getUser2() {
+        return user2;
+    }
+
+    public void setUser2(String user2) {
+        this.user2 = user2;
     }
 }
