25dd968253c8aecebcdbe88f38ceaeaaf3de7771
SteveDexter24 <tamangsteve@gmail.com>
2022-05-15 23:18:57 +0800

    Get the chat room of current user

--- app/src/main/java/com/yolo/ecosell/ChatListActivity.java
+++ app/src/main/java/com/yolo/ecosell/ChatListActivity.java
@@ -1,49 +1,114 @@
 package com.yolo.ecosell;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.yolo.ecosell.adapter.ChatAdapter;
 import com.yolo.ecosell.adapter.ListingRecyclerViewAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import model.ChatRoom;
+import model.User;
+import model.UserViewModel;
+
 public class ChatListActivity extends AppCompatActivity {
+    private final String TAG = "ChatListActivity";
 
     private String username;
     private String email;
+    private UserViewModel userViewModel;
 
+    // Views
     private RecyclerView chatRecyclerView;
     private ChatAdapter chatRecyclerViewAdapter;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference chatCollectionReference = db.collection("Chats");
+    private ImageView profileImageView, backImageView;
 
+    // Firebase
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference chatRoomsCollectionReference = db.collection("ChatRooms");
+    private CollectionReference userCollectionReference = db.collection("Users");
+    private User user;
+    private List<ChatRoom> chatRoomList;
+
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list);
-        //Objects.requireNonNull(getSupportActionBar()).setTitle("Messages");
+
+        chatRoomList = new ArrayList<>();
+
         getSupportActionBar().hide();
+        profileImageView = findViewById(R.id.chat_list_activity_ImageView);
+        backImageView = findViewById(R.id.chat_list_back_image);
+
+        backImageView.setOnClickListener(view -> {
+            startActivity(new Intent(ChatListActivity.this, HomeActivity.class));
+        });
+
+        userViewModel = new ViewModelProvider.AndroidViewModelFactory(ChatListActivity.this.getApplication())
+                .create(UserViewModel.class);
+        userViewModel.getAllUsers().observe(this, users -> {
+            Glide.with(this).load(users.get(0).getImageUrl()).into(profileImageView);
+        });
+        firebaseAuth = FirebaseAuth.getInstance();
 
         chatRecyclerView = findViewById(R.id.chat_list_RecyclerView);
         chatRecyclerView.setHasFixedSize(true);
         chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // Setup adapter
-        chatRecyclerViewAdapter = new ChatAdapter(this);
-
-        chatRecyclerView.setAdapter(chatRecyclerViewAdapter);
-
-        chatRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
     }
 
-
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        chatRoomsCollectionReference
+                .whereEqualTo("user", currentUser.getUid())
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (!queryDocumentSnapshots.isEmpty()) {
+                        for (QueryDocumentSnapshot chatRooms : queryDocumentSnapshots) {
+                            ChatRoom chatRoom = chatRooms.toObject(ChatRoom.class);
+                            chatRoomList.add(chatRoom);
+                            // Setup adapter
+                            chatRecyclerViewAdapter = new ChatAdapter(this, chatRoomList, chatRooms.getId());
+                            chatRecyclerView.setAdapter(chatRecyclerViewAdapter);
+                            chatRecyclerViewAdapter.notifyDataSetChanged();
+                        }
+                    } else {
+                        Log.d(TAG, "chat room is empty");
+                    }
+                });
+    }
 }
