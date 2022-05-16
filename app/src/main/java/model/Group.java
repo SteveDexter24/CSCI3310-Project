54a406cfc66fa5ffcfc5886f5ceb62aaed960503
SteveDexter24 <tamangsteve@gmail.com>
2022-05-16 20:45:59 +0800

    creator ID instead of document ref

--- app/src/main/java/model/Group.java
+++ app/src/main/java/model/Group.java
@@ -10,14 +10,16 @@ public class Group {
     private Timestamp createdTime;
     private String groupName;
     private String groupDescription;
-    private DocumentReference creator;
+    private String creator;
+    private String groupImageUrl;
     private List<DocumentReference> users;
 
-    public Group(Timestamp createdtime, String groupName, String groupDescription, DocumentReference creator, List<DocumentReference> users){
+    public Group(Timestamp createdtime, String groupName, String groupDescription, String creator, String groupImageUrl, List<DocumentReference> users){
         this.createdTime = createdtime;
         this.groupName = groupName;
         this.groupDescription = groupDescription;
         this.creator = creator;
+        this.groupImageUrl = groupImageUrl;
         this.users = users;
     }
 
@@ -30,8 +32,12 @@ public class Group {
     public String getGroupDescription() { return groupDescription; }
     public void setGroupDescription(String groupDescription) { this.groupDescription = groupDescription; }
 
-    public DocumentReference getCreator() { return creator; }
-    public void setCreator(DocumentReference creator) { this.creator = creator; }
+    public String getCreator() { return creator; }
+    public void setCreator(String creator) { this.creator = creator; }
+
+    public String getGroupImageUrl() { return groupImageUrl; }
+
+    public void setGroupImageUrl(String groupImageUrl) { this.groupImageUrl = groupImageUrl; }
 
     public List<DocumentReference> getUsers() { return users; }
     public void setUsers(List<DocumentReference> users) { this.users = users; }
