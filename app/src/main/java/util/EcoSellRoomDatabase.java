6dbc19051d8ac07cecc9eaeae39ead4279ac4913
SteveDexter24 <tamangsteve@gmail.com>
2022-05-17 16:19:21 +0800

    Change DB version and add groupDao

--- app/src/main/java/util/EcoSellRoomDatabase.java
+++ app/src/main/java/util/EcoSellRoomDatabase.java
@@ -12,13 +12,16 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import data.GroupDao;
 import data.UserDao;
+import model.Group;
 import model.User;
 
-@Database(entities = {User.class}, version = 2, exportSchema = false)
+@Database(entities = {User.class, Group.class}, version = 3, exportSchema = false)
 @TypeConverters({DataConverter.class})
 public abstract class EcoSellRoomDatabase extends RoomDatabase {
     public abstract UserDao userDao();
+    public abstract GroupDao groupDao();
     public static final int NUM_OF_THREADS = 4;
 
     private static volatile EcoSellRoomDatabase INSTANCE;
@@ -45,6 +48,7 @@ public abstract class EcoSellRoomDatabase extends RoomDatabase {
                     // Away from UI Thread
                     databaseWriteExecutor.execute(() -> {
                         UserDao userDao = INSTANCE.userDao();
+                        GroupDao groupDao = INSTANCE.groupDao();
                         userDao.deleteAll();
                     });
                 }
