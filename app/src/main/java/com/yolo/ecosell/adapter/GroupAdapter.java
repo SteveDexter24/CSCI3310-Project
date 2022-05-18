78c14676f7ca2eaca1a2fa7b4bca8fd1fecafd19
SteveDexter24 <tamangsteve@gmail.com>
2022-05-18 08:31:31 +0800

    intent to group detail

--- app/src/main/java/com/yolo/ecosell/adapter/GroupAdapter.java
+++ app/src/main/java/com/yolo/ecosell/adapter/GroupAdapter.java
@@ -1,6 +1,7 @@
 package com.yolo.ecosell.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +9,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.yolo.ecosell.GroupDetailActivity;
 import com.yolo.ecosell.R;
 
 import java.util.List;
@@ -44,7 +47,13 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
                 .load(group.getGroupImageUrl()).into(holder.group_item_image);
 
         // Intent to group detailed view
-
+        holder.groupCardView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, GroupDetailActivity.class);
+            intent.putExtra("itemAtIndex", position);
+            intent.putExtra("title", group.getGroupName());
+            intent.putExtra("groupId", group.getGroupId());
+            context.startActivity(intent);
+        });
     }
 
     @Override
@@ -55,6 +64,7 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
     public class GroupViewHolder extends RecyclerView.ViewHolder{
         private ImageView group_item_image;
         private TextView group_item_name, group_item_description;
+        private CardView groupCardView;
 
         public GroupViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -62,6 +72,7 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
             group_item_image = itemView.findViewById(R.id.group_item_image);
             group_item_name = itemView.findViewById(R.id.group_item_name);
             group_item_description = itemView.findViewById(R.id.group_item_description);
+            groupCardView = itemView.findViewById(R.id.group_item_card_view);
 
         }
 
