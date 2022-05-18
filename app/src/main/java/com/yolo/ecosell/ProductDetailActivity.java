ebd73560f71d80018fd7bb95c9bc15a70cd85a0e
SteveDexter24 <tamangsteve@gmail.com>
2022-05-19 04:27:10 +0800

    Add like and un like feature

--- app/src/main/java/com/yolo/ecosell/ProductDetailActivity.java
+++ app/src/main/java/com/yolo/ecosell/ProductDetailActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +25,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -52,6 +54,9 @@ public class ProductDetailActivity extends AppCompatActivity {
     private User user;
     private String userId, productId, productPrice, condition, productCategory, productDescription, productDeliveryMethod;
     private String productSeller, productBuyer, imageUrl;
+    private ImageButton likeButton;
+    private int heartNotLiked, heartLiked;
+    private boolean likeClicked = false;
 
     private ImageView productImageView;
     private TextView productNameTextView, productPriceTextView, productSellerTextView,
@@ -61,6 +66,7 @@ public class ProductDetailActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference chatRoomsCollectionReference = db.collection("ChatRooms");
     private CollectionReference userCollectionReference = db.collection("Users");
+    private CollectionReference productCollectionReference = db.collection("Products");
 
 
     @Override
@@ -84,6 +90,10 @@ public class ProductDetailActivity extends AppCompatActivity {
         productCategoryTextView = findViewById(R.id.productCategory);
         productDescriptionTextView = findViewById(R.id.productDescription);
         deliveryMethodTextView = findViewById(R.id.productDeliveryMethod);
+        likeButton = findViewById(R.id.likeButton);
+
+        heartLiked = R.drawable.ic_baseline_favorite_24_red;
+        heartNotLiked = R.drawable.ic_outline_favorite_border_24;
 
         // floating action buttons;
         chatFloatingButton = findViewById(R.id.product_detail_chat_floating_action);
@@ -95,11 +105,20 @@ public class ProductDetailActivity extends AppCompatActivity {
             for (Product p : products) {
                 if (p.getProductId().equals(productId)) {
                     product = p;
+                    likeClicked = product.getLikes().contains(userId);
+
+                    if (likeClicked) {
+                        likeButton.setImageResource(heartLiked);
+                    } else {
+
+                        likeButton.setImageResource(heartNotLiked);
+                    }
                     setDataToUI(product);
                     break;
                 }
             }
         });
+
 
         userViewModel = new ViewModelProvider.AndroidViewModelFactory(this.getApplication())
                 .create(UserViewModel.class);
@@ -114,6 +133,20 @@ public class ProductDetailActivity extends AppCompatActivity {
             createChatRoom();
         });
 
+        likeButton.setOnClickListener(view -> {
+
+            if (likeClicked) {
+                likeButton.setImageResource(heartNotLiked);
+                likeClicked = false;
+                // pull from list
+                removeLikes();
+            } else {
+                likeButton.setImageResource(heartLiked);
+                likeClicked = true;
+                // insert to list
+                addLikes();
+            }
+        });
     }
 
     private void setDataToUI(Product product) {
@@ -126,8 +159,53 @@ public class ProductDetailActivity extends AppCompatActivity {
         productCategoryTextView.setText(product.getProductCategory());
         productDescriptionTextView.setText(product.getProductDescription());
         deliveryMethodTextView.setText(product.getProductDeliveryMethod());
-
     }
+
+    private void addLikes() {
+        productCollectionReference
+                .document(productId)
+                .update("likes", FieldValue.arrayUnion(userId))
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+
+                        userCollectionReference
+                                .document(userId)
+                                .update("likes", FieldValue.arrayUnion(productId))
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        Toast.makeText(this, "Added to likes", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                    }
+                })
+                .addOnFailureListener(e -> {
+
+                });
+    }
+
+    private void removeLikes() {
+        productCollectionReference
+                .document(productId)
+                .update("likes", FieldValue.arrayRemove(userId))
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+
+                        userCollectionReference
+                                .document(userId)
+                                .update("likes", FieldValue.arrayRemove(productId))
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        Toast.makeText(this, "Removed from likes", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(e -> {
+
+                });
+    }
+
 
     private void createChatRoom() {
         //String chatRoomId = chatRoomsCollectionReference.document().getId();
@@ -151,7 +229,7 @@ public class ProductDetailActivity extends AppCompatActivity {
                 .whereNotIn("users", Arrays.asList(userId, sellerId))
                 .get()
                 .addOnSuccessListener(queryDocumentSnapshots -> {
-                    if (queryDocumentSnapshots.isEmpty()){
+                    if (queryDocumentSnapshots.isEmpty()) {
                         chatRoomDocRef
                                 .set(chatRoom, SetOptions.merge())
                                 .addOnCompleteListener(task -> {
@@ -160,7 +238,7 @@ public class ProductDetailActivity extends AppCompatActivity {
                                         startActivity(intent);
                                     }
                                 });
-                    }else {
+                    } else {
                         Toast.makeText(this, "You already have a chat room with this user", Toast.LENGTH_LONG).show();
                         Intent intent = new Intent(this, ChatListActivity.class);
                         startActivity(intent);
